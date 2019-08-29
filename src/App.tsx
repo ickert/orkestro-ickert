6630dfd8c44992aaa223bee05ae8b033418f75f6
Felipe Ickert <fvickert@gmail.com>
2019-08-29 21:48:54 +0200

    added styled-component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: green;
+`
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Button>audhaudasd</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
